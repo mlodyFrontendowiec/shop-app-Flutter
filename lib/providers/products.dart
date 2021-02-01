10bc9f851bd04f4066b15d3365f8a49a148ba4be
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-02-01 21:06:21 +0100

    Add provider

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:shop/models/product.dart';
+import 'package:shop/providers/product.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = [
@@ -40,6 +40,10 @@ class Products with ChangeNotifier {
 
   List<Product> get items {
     return [..._items];
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
   }
 
   void addProduct() {
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:shop/models/product.dart';
-import 'package:shop/widgets/product_item.dart';
 import 'package:shop/widgets/products_grid.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
